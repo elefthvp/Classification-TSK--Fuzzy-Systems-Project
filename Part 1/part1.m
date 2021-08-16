82d8a45a4e2c772fd7d301e8ae4cd583ad41f5bc
elefthvp <68590451+elefthvp@users.noreply.github.com>
2021-08-17 01:33:11 +0300

    Update part1.m

--- Part 1/part1.m
+++ Part 1/part1.m
@@ -1,9 +1,8 @@
 %Eleftheria Papaioannou AEM:8566
-%Fuzzy Systems, exercise 4 part 1
 
 clear;
 close all;
-%help SUBCLUST
+
 load avila.txt
 data=avila;
 
@@ -22,31 +21,20 @@ valDatax=data(b,1:end-1);
 valDatay=data(b,end);
 chkDatax=data(c,1:end-1);
 chkDatay=data(c,end);
-%num_of_tsk=5;
-%NR_ideal = [4,8,12,16,20];
+
 NR=[2 3 4 5];
 num_of_tsk=length(NR);
 
 
 rad=[0.55 0.25 0.23 0.2 0.195 0.19 0.185 0.18];
-%rad2=[0.5 0.45 0.4 0.35 0.3 0.25 0.22]
 k=1
 num_of_rules=[];
 
 
 for i=1:length(rad)
-%i=1
     model(i)= genfis2(trnDatax,trnDatay,rad(i));
     rules=length(model(i).rule);
     num_of_rules=[num_of_rules length(model(i).rule)];
-%     if(rules==NR(k))
-%         final_model(k)=model;
-%         k=k+1;
-%         if(k>num_of_tsk)
-%         break; %%all models found
-%         end
-%     end
-    
  end
 
 trnOpt = NaN(5, 1);
@@ -59,8 +47,6 @@ TrnOpt(3)=0.01;
 
 
 for i=1:num_of_tsk
-%i=1;
-
      n(i) = getfis(model(i), 'NumOutputMFs');
      for j = 1:n(i)
        oldParams = model(i).output.mf(j).('params');
@@ -92,15 +78,14 @@ for i=1:num_of_tsk
 
        subplot(4,3,j)
        title ('model with 2 rules' )
-        [f,mf] = plotmf(chkFis,'input',j);
+       [f,mf] = plotmf(chkFis,'input',j);
         %filename = ['In' num2str(j) 'mod' num2str(NR(i)) '.jpg']
-        plot(f ,mf)
-        title(['Input ' num2str(j)], 'fontweight','bold','fontsize',10)
+       plot(f ,mf)
+       title(['Input ' num2str(j)], 'fontweight','bold','fontsize',10)
         %saveas(1,filename)
-        
     end
      
-      %filename = ['learning curve' num2str(NR(i)) '.jpg']
+    %filename = ['learning curve' num2str(NR(i)) '.jpg']
     figure();
     plot(error, 'LineWidth',2)
     hold on
